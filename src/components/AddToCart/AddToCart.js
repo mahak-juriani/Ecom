a42afaeb24328510f646e999df83892e60ab6c98
Mahak Juriani <juriani.mahak@gmail.com>
2024-06-24 09:28:12 +0530

    memo

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,6 +1,8 @@
-function AddToCart({ product,  cart, increaseQuantity, decreaseQuantity }) {
-    // inc
-    // dec
+import { useContext } from "react";
+import CartContext from "../../context/CartContext";
+function AddToCart({ product }) {
+    console.log("add to cart", product.id)
+    const { cart, increaseQuantity, decreaseQuantity } = useContext(CartContext);
     function increase() {
         increaseQuantity(product);
     }
